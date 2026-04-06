134d60b1570a804d36c5fc72664789326f3c882f
Final Prescription <final.prescriptio.aips@gmail.com>
2026-04-06 13:43:23 +0000

    fix: add basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,10 @@
-import type {NextConfig} from 'next';
 
-const repo = 'farewell'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: '/Farewell',
+  assetPrefix: '/Farewell',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
   },
 };
 
