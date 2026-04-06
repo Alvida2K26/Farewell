a802a515911001c095ec4283331b82606235e4c4
Final Prescription <final.prescriptio.aips@gmail.com>
2026-04-06 09:41:31 +0000

    Launch

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ['https://6000-firebase-studio-1775365666384.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev'],
-  },
   images: {
     remotePatterns: [
       {
